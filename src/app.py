b0008d9d131ae23607d5952a37bfc451d0e19037
Killer66562 <jimmy688382@gmail.com>
2024-07-29 03:07:22 +0800

    DO NOT RELEASE YOUR TOKEN!

--- src/app.py
+++ src/app.py
@@ -3,6 +3,12 @@ from discord import Intents, Status
 
 from cogs import SurveyCog
 
+from dotenv import load_dotenv
+
+import os
+
+
+load_dotenv()
 
 health_bot = Bot(command_prefix="health~", description="智能健康管家", intents=Intents.all())
 
@@ -14,4 +20,4 @@ async def on_ready():
     await health_bot.tree.sync()
     await health_bot.change_presence(status=Status.online)
 
-health_bot.run(token="MTI2NjM4MzIyNDg4ODYyMzE1NQ.GWw_pp.Ed4PMVGx0YOEY7qb5x0W6H0ujnMfOtK7KUOfgM", reconnect=True)
+health_bot.run(token=os.getenv("TOKEN"), reconnect=True)
